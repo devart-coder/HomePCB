1bb454c3ef1c99f424c3115c2da5e92271144d12
Devart <devart.mail@mail.ru>
2025-11-21 02:58:50 +0300

    Delete build directory

--- build/Desktop-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_HomePCB_FILE /home/devart/Development/Cpp/HomePCB/build/Desktop-Debug/HomePCB)
-set(__QT_DEPLOY_TARGET_HomePCB_TYPE EXECUTABLE)
--- build/Desktop-Release/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_untitled_FILE /home/devart/Development/Cpp/HomePCB/build/Desktop-Release/untitled)
-set(__QT_DEPLOY_TARGET_untitled_TYPE EXECUTABLE)
